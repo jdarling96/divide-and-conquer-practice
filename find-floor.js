ae6f2e0a93db59e56328b6775c5405ca1c393809
Josh Darling <joshua.a.darling@gmail.com>
2022-12-01 11:15:03 -0700

    problems completed all tests passed

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,27 @@
-function findFloor() {
-  
+function findFloor(arr, val) {
+  let leftIdx = 0;
+  let rightIdx = arr.length - 1;
+  while (leftIdx <= rightIdx) {
+    let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+    let middleVal = arr[middleIdx];
+    if (middleVal < val && arr[rightIdx] > val) {
+      return middleVal;
+    }
+    if (middleIdx === leftIdx && middleIdx === rightIdx) {
+      if (middleVal <= val) {
+        return middleVal;
+      } else {
+        return -1;
+      }
+    }
+    if (middleVal < val) {
+      leftIdx = middleIdx + 1;
+    }
+
+    if (middleVal > val) {
+      rightIdx = middleIdx - 1;
+    }
+  }
 }
 
-module.exports = findFloor
+module.exports = findFloor;
